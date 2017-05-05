862339d2fa312d47850785a859298591cd876d7e
Dima Polovinchenko <polovinchenkodima@gmail.com>
2017-05-05 22:15:06 +0300

    adding browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-var gulp 			= require('gulp'), // Connect Gulp
-		concatCSS = require('gulp-concat-css'), // Merge all Css to one
-		rename		=	require('gulp-rename'), // Rename our Css
-		notify		=	require('gulp-notify'), // Makes beautifull notifications
-		prefix		=	require('gulp-autoprefixer'), // Puts autoprefix to css properties
-		minifyCSS = require('gulp-minify-css'), // Minimize Css
-		sass			= require('gulp-sass'); 
+var gulp 				= require('gulp'), // Connect Gulp
+		browserSync = require('browser-sync').create(), // Include browserSync
+		concatCSS 	= require('gulp-concat-css'), // Merge all Css to one
+		rename			=	require('gulp-rename'), // Rename our Css
+		notify			=	require('gulp-notify'), // Makes beautifull notifications
+		prefix			=	require('gulp-autoprefixer'), // Puts autoprefix to css properties
+		minifyCSS 	= require('gulp-minify-css'); // Minimize Css
  
 gulp.task('default', function () {
   gulp.src('app/css/*.css') // Collecting all Css
@@ -13,9 +13,20 @@ gulp.task('default', function () {
     .pipe(minifyCSS('')) // Compressing Css
     .pipe(rename('bundle.min.css')) // Rename our Css
     .pipe(gulp.dest('app/css')) // Unload Css
+    .pipe(browserSync.reload({stream: true})) // Add automatic reload
     .pipe(notify('Done!')); // If success alert Done!
 });
 
-gulp.task('watch', function () {
-	gulp.watch('css/*.css', ['default']) // Start to watch for changes in .css
+gulp.task('browser-sync', function() { // Create task for browser-sync
+    browserSync.init({ // Perform browser-sync
+        server: { // Define server parameters
+            baseDir: "app" // Server dir
+        }
+    });
+});
+
+gulp.task('watch', ['browser-sync'], function () {
+	gulp.watch('css/*.css', ['css']) // Start to watch for changes in .css
+	gulp.watch('app/*.html', browserSync.reload) // Start to watch for changes in .html
+	gulp.watch('app/js/**/*.js', browserSync.reload) // Start to watch for changes in .js
 });
